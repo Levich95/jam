ca8c8c23f1853e40cd42cb7d13f829916cc76e50
Lev Usenko <lyusen@yahoo.com>
2020-03-22 00:10:32 +0300

    fix: discussions

--- migrations/initial.sql
+++ migrations/initial.sql
@@ -1,12 +1,13 @@
-drop schema if exists jam;
-
 create schema if not exists jam;
 
 create table jam.profiles (
-  id serial not null,
+  id serial,
   login varchar(16) not null,
   password varchar(16) not null,
   name varchar(16),
+  created timestamp not null default now(),
+  updated timestamp,
+  archived boolean not null default false,
 
   primary key id
 );
@@ -14,35 +15,50 @@ create table jam.profiles (
 create unique index profiles_login_idx on jam.profiles login;
 
 create table jam.events (
-  id serial not null,
+  id serial,
   title varchar(64) not null,
-  date date not null,
+  date timestamp not null,
   profile_id integer not null,
+  created timestamp not null default now(),
+  updated timestamp,
+  archived boolean not null default false,
 
   primary key id,
   foreign key profile_id references jam.profiles
 );
 
 create table jam.tracks (
-  id serial not null,
+  id serial,
   title varchar(64) not null,
   event_id integer not null,
+  created timestamp not null default now(),
+  updated timestamp,
+  archived boolean not null default false,
 
   primary key id,
   foreign key event_id references jam.events
 );
 
 create table jam.part_types (
-  id serial not null,
-  title varchar(64) not null
+  id serial,
+  title varchar(64) not null,
+  created timestamp not null default now(),
+  updated timestamp,
+  archived boolean not null default false,
+
+  primary key id
 );
 
 create table jam.parts (
-  id serial not null,
+  id serial,
   profile_id integer not null,
   track_id integer not null,
   part_type_id integer not null,
+  created timestamp not null default now(),
+  updated timestamp,
+  archived boolean not null default false,
 
+  primary key id,
   foreign key profile_id references jam.profiles,
   foreign key track_id references jam.tracks,
   foreign key part_type_id references jam.part_types
